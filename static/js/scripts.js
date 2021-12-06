8365c0f3efbf92a0fb05f84c02e18ccbdb8aab4e
Onoma94 <artur.gierach@wp.pl>
2021-12-06 19:55:12 +0100

    Rewritten to use local storage to store orders (BROKEN!). Added comments.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,39 @@
-"use strict";
+function removeFromLocalStorage(pizza)
+{
+    localStorage.removeItem(pizza);
+}
+function clearOrders()
+{
+    localStorage.clear();
+}
 
+/* reloads the orders after page reload or refresh */
+function fillOrders()
+{
+    console.log(localStorage);
+    if(localStorage.length == 0)
+    {        /* do nothing */    }
+    else
+    {
+        let stored_keys = Object.keys(localStorage).filter(k => k.startsWith('Ó'));
+        for(var i = 0; i < stored_keys.length; i++)
+        {
+            var title = stored_keys[i].substring(1);
+            console.log(title);
+            if(!(document.getElementById(title)))
+            {
+                console.log(document.getElementById(title));
+                addPizzaElement(document.getElementById(title), localStorage.getItem(stored_keys[i]));
+            }
+        }
+    }
+}
+
+/* listing available pizzae in the menu */
 function listPizzae()
 {
     var newDiv = null;
+    
     fetch('https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json')
     .then(response => response.json())
     .then(data => 
@@ -20,27 +51,24 @@ function listPizzae()
                 +'<div class="title-price"><div class="title">'+pizza.title+'</div>'
                 +'<div class="price">'+(pizza.price).toLocaleString("pl-PL")+'0 zł</div></div>'
                 +'<ingr>'+ingredients1+'</ingr>'
-                +'<button onClick="addPizza(this);calculateOrders()">Zamów</button>';
+                +'<button onClick="addPizzaButton(this);calculateOrders()">Zamów</button>';
             document.getElementById("pizzae").appendChild(newDiv);
-        }));
+        }))
+        .then(fillOrders());
 }
 
-function addPizza(e)
+/* adds a div with pizza to the orders */
+/* the arguments are: the menu <pizza> element, and how many pizzae of this kind is being ordered */
+function addPizzaElement(pizza, count)
 {
-    var newDiv = null;
-    const basket = document.getElementById("orders");
-    var id = e.parentNode.id;
-    var title = e.parentNode.querySelector("div.title-price div.title").innerHTML;
-    var price = e.parentNode.querySelector("div.title-price div.price").innerHTML;
-    var count = 1;
-    if(!(document.getElementById("O"+id)))
-    {
-        newDiv = document.createElement("order");
-        newDiv.setAttribute("id", "O"+title);
+    var title = pizza.querySelector("div.title-price div.title").innerHTML;
+    var price = pizza.querySelector("div.title-price div.price").innerHTML;
+        let newDiv = document.createElement("order");
+        newDiv.setAttribute("id", "Ó"+title);
         newDiv.innerHTML = '<div class="order-title">'+title+'</div>'
             + '<div class="order-price">'+price+'</div>'
             + '<div class="order-count">' +count+ '</div>'
-            + '<div class="order-delete" onClick="removePizza(this);calculateOrders()">×</div>';
+            + '<div class="order-delete" onClick="removePizzaElement(this);calculateOrders()">×</div>';
         document.getElementById("orders").appendChild(newDiv);
         if(!!document.getElementById("basket-empty"))
         {
@@ -49,14 +77,60 @@ function addPizza(e)
             var basket_price = document.getElementById("basket-price");
             basket_price.style.display = "block";
         }
+}
+
+/* adds a pizza from the button in menu to the orders */
+function addPizzaButton(button)
+{
+    
+    //const basket = document.getElementById("orders");
+    
+    //var title = button.parentNode.querySelector("div.title-price div.title").innerHTML;
+    //var price = button.parentNode.querySelector("div.title-price div.price").innerHTML;
+    var title = button.parentNode.id;
+    var count = 1;
+    if(!(document.getElementById("Ó"+title)))
+    {
+        addPizzaElement(button.parentNode, count);
+        localStorage.setItem("Ó"+title, 1);
     }
     else
     {
-        var count = document.getElementById("O"+id).querySelector("div.order-count").innerHTML;
+        var count = document.getElementById("Ó"+title).querySelector("div.order-count").innerHTML;
         count++;
-        document.getElementById("O"+id).querySelector("div.order-count").innerHTML = count;
+        document.getElementById("Ó"+title).querySelector("div.order-count").innerHTML = count;
+        localStorage.setItem("Ó"+title, count);
     }
 }
+
+/* removes one piece of pizza from the orders */
+function removePizzaElement(button)
+{
+    var count = parseInt(button.parentNode
+        .querySelector("div.order-count").innerHTML);
+    count--;
+    button.parentNode.querySelector("div.order-count").innerHTML = count;
+    if (!count)
+    {
+        let order = button.parentNode;
+        order.parentNode.removeChild(order);
+        localStorage.removeItem(order);
+        if (document.getElementsByTagName("order").length < 1)
+        {
+            var basket_empty = document.getElementById("basket-empty");
+            basket_empty.style.display = "block";
+            var basket_price = document.getElementById("basket-price");
+            basket_price.style.display = "none";
+        }
+    }
+    else
+    {
+        localStorage.setItem(button.parentNode.id,count);
+    }
+}
+
+
+/* calculating the total cost of the orders */
 function calculateOrders()
 {
     var total_price = 0;
@@ -70,23 +144,4 @@ function calculateOrders()
         total_price = total_price + a*b;
     }
     document.getElementById("basket-price-number").innerHTML = total_price.toFixed(1).replace(".",",")+"0 zł";
-}
-function removePizza(e)
-{
-    var count = parseInt(e.parentNode
-        .querySelector("div.order-count").innerHTML);
-    count--;
-    e.parentNode.querySelector("div.order-count").innerHTML = count;
-    if (!count)
-    {
-        let id = e.parentNode;
-        id.parentNode.removeChild(id);
-        if (document.getElementsByTagName("order").length < 1)
-        {
-            var basket_empty = document.getElementById("basket-empty");
-            basket_empty.style.display = "block";
-            var basket_price = document.getElementById("basket-price");
-            basket_price.style.display = "none";
-        }
-    }
 }
---
+++ static/js/scripts_old.js
@@ -0,0 +1,100 @@
+"use strict";
+
+/* listing pizzae in the menu */
+function listPizzae()
+{
+    var newDiv = null;
+    fetch('https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json')
+    .then(response => response.json())
+    .then(data => 
+        data.forEach(pizza => {
+            newDiv = document.createElement("pizza");
+            newDiv.setAttribute("id", pizza.title);
+            var ingredients1 = "";
+            pizza.ingredients.forEach(ingredient =>
+                {
+                    ingredients1 = ingredients1 + ingredient + ", ";
+                }
+            );
+            ingredients1 = ingredients1.slice(0,-2)
+            newDiv.innerHTML = '<img src="'+pizza.image+'">'
+                +'<div class="title-price"><div class="title">'+pizza.title+'</div>'
+                +'<div class="price">'+(pizza.price).toLocaleString("pl-PL")+'0 zł</div></div>'
+                +'<ingr>'+ingredients1+'</ingr>'
+                +'<button onClick="addPizza(this);calculateOrders()">Zamów</button>';
+            document.getElementById("pizzae").appendChild(newDiv);
+        }));
+}
+
+/* adding an order */
+function addPizza(e)
+{
+    var newDiv = null;
+    const basket = document.getElementById("orders");
+    var id = e.parentNode.id;
+    var title = e.parentNode.querySelector("div.title-price div.title").innerHTML;
+    var price = e.parentNode.querySelector("div.title-price div.price").innerHTML;
+    var count = 1;
+    if(!(document.getElementById("O"+id)))
+    {
+        newDiv = document.createElement("order");
+        newDiv.setAttribute("id", "O"+title);
+        newDiv.innerHTML = '<div class="order-title">'+title+'</div>'
+            + '<div class="order-price">'+price+'</div>'
+            + '<div class="order-count">' +count+ '</div>'
+            + '<div class="order-delete" onClick="removePizza(this);calculateOrders()">×</div>';
+        document.getElementById("orders").appendChild(newDiv);
+        if(!!document.getElementById("basket-empty"))
+        {
+            var basket_empty = document.getElementById("basket-empty");
+            basket_empty.style.display = "none";
+            var basket_price = document.getElementById("basket-price");
+            basket_price.style.display = "block";
+        }
+    }
+    else
+    {
+        var count = document.getElementById("O"+id).querySelector("div.order-count").innerHTML;
+        count++;
+        document.getElementById("O"+id).querySelector("div.order-count").innerHTML = count;
+    }
+}
+
+/* removing an order */
+
+function removePizza(e)
+{
+    var count = parseInt(e.parentNode
+        .querySelector("div.order-count").innerHTML);
+    count--;
+    e.parentNode.querySelector("div.order-count").innerHTML = count;
+    if (!count)
+    {
+        let id = e.parentNode;
+        id.parentNode.removeChild(id);
+        if (document.getElementsByTagName("order").length < 1)
+        {
+            var basket_empty = document.getElementById("basket-empty");
+            basket_empty.style.display = "block";
+            var basket_price = document.getElementById("basket-price");
+            basket_price.style.display = "none";
+        }
+    }
+}
+
+/* calculating the total cost of the orders */
+
+function calculateOrders()
+{
+    var total_price = 0;
+    var a, b;
+    var prices = document.getElementsByClassName("order-price");
+    var counts = document.getElementsByClassName("order-count");
+    for (let index = 0; index < prices.length; index++) 
+    {
+        a = parseFloat(prices[index].innerHTML.replace(",",".").replace(" zł",""));
+        b = parseInt(counts[index].innerHTML);
+        total_price = total_price + a*b;
+    }
+    document.getElementById("basket-price-number").innerHTML = total_price.toFixed(1).replace(".",",")+"0 zł";
+}
